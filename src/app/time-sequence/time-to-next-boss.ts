6d53a1936418546b7ed059f244d2e33f871c255f
Arturas Molcanovas <a.molcanovas@gmail.com>
2020-06-03 22:05:01 +0100

    fix: timer now stringified consistently

--- src/app/time-sequence/time-to-next-boss.ts
+++ src/app/time-sequence/time-to-next-boss.ts
@@ -3,12 +3,8 @@ import {Observable, of, timer} from 'rxjs';
 import {distinctUntilChanged, map, shareReplay, switchMap} from 'rxjs/operators';
 import {NEXT_DATE_BF_ADJUST$} from './next-date';
 
-function reducer(acc: string[], num: number): string[] {
-  if (num) {
-    acc.push(num.toString().padStart(2, '0'));
-  }
-
-  return acc;
+function innerMapper(n: number): string {
+  return n.toString().padStart(2, '0');
 }
 
 export const TIME_TO_NEXT_BOSS$: Observable<string | null> = NEXT_DATE_BF_ADJUST$.pipe(
@@ -22,7 +18,7 @@ export const TIME_TO_NEXT_BOSS$: Observable<string | null> = NEXT_DATE_BF_ADJUST
         const dur = moment.duration(d.diff(moment(), 'seconds'), 'seconds');
 
         return [dur.hours(), dur.minutes(), dur.seconds()]
-          .reduce(reducer, [])
+          .map(innerMapper)
           .join(':');
       })
     );
